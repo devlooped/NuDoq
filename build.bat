6d95f8faa87b5aec02bb93f6ce0334cad91cac98
Daniel Cazzulino <daniel@cazzulino.com>
2014-09-02 14:07:17 -0300

    Simplified build, removed submodule and nuget stuff

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\src\NuDoq\NuGet.proj" /verbosity:normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild %~dp0\build.proj /verbosity:normal /nr:false

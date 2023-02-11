8680df239693e87b586214d0b7b32d051193e864
Daniel Cazzulino <daniel@cazzulino.com>
2023-02-11 02:19:43 +0000

    Add SponsorLink to ensure ongoing development and maintenance
    See https://github.com/devlooped/SponsorLink and
    https://www.cazzulino.com/sponsorlink.html.

---
+++ src/NuDoq.CodeAnalysis/SponsorLinker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Devlooped;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace NuDoq;
+
+[Generator]
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic, LanguageNames.FSharp)]
+class SponsorLinker : SponsorLink
+{
+    public SponsorLinker() : base(SponsorLinkSettings.Create(
+        "devlooped", "NuDoq",
+        version: new Version(ThisAssembly.Info.Version).ToString(2)
+#if DEBUG
+        , quietDays: 0
+#endif
+        ))
+    { }
+}
